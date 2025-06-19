da08cb0e2959f7d2691aca64455412dc03e314a2
gopal awasthi <gopalawasthiji@gmail.com>
2025-06-19 11:13:49 +0530

    [Gopal Awasthi] fix class name typo in ReverseNumberArray

--- Arrays/ReverseNumberArray.java
+++ Arrays/ReverseNumberArray.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class ReversenArray {
+public class ReverseNumberArray {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter a n");
