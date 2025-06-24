ac9a8f8b4ad20d498d89347362f81503a22d0c95
gopal awasthi <gopalawasthiji@gmail.com>
2025-06-24 18:38:45 +0530

    Clean main branch by removing all files and folders

--- OOPS/Class_Object/MainEmployee.java
+++
@@ -1,8 +0,0 @@
-package com.classobject;
-public class MainEmployee {
-
-	public static void main(String[] args) {
-		 Employee employee = new Employee("himanshu raghav", 9, 4000000);
-	        employee.displayDetails(); 		
-	}
-}
